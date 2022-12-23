76d16a0e26c45a4a7175772f3294dd8071b808e0
Distortions81 <carlotto81@gmail.com>
2022-12-22 20:59:36 -0700

    prevent map use

--- loop.lua
+++ loop.lua
@@ -9,8 +9,11 @@ script.on_event(defines.events.on_tick, function(event)
 
     -- Check how many players are online
     local numplayers = 0
-    for _, _ in pairs(game.connected_players) do
+    for _, victim in pairs(game.connected_players) do
         numplayers = numplayers + 1
+        if victim.render_mode ~= defines.render_mode.game then
+            victim.close_map()
+        end
     end
 
     -- Detect player count change
@@ -84,7 +87,7 @@ script.on_event(defines.events.on_tick, function(event)
                 -- If above minimum, damage player
                 if global.d_player_dmg[player.index] and global.d_player_dmg[player.index] > 5 then
                     -- player.print("[color=red]The darkness gnaws at you...[/color]")
-                    player.play_sound ({
+                    player.play_sound({
                         path = "dark-damage",
                         position = player.character.position,
                         volume_modifier = 1
