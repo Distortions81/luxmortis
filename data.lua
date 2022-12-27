7e4738be00bd1ede0088e128348d8768fc36aa31
carlotto81 <carlotto81@gmail.com>
2022-12-27 12:41:41 -0700

    remove old recipie

--- data.lua
+++ data.lua
@@ -1,29 +1,4 @@
-local vanilla = "small-lamp"
-local lamp_name = "burner-lamp"
 
-local entity = table.deepcopy(data.raw.lamp[vanilla])
-entity.name = lamp_name
-entity.energy_source = {type = "void"}
-entity.localised_name = {"entity-name."..lamp_name}
-entity.localised_description = {"entity-description."..lamp_name}
-
-local recipe = table.deepcopy(data.raw.recipe[vanilla])
-recipe.name = lamp_name
-recipe.results = { {type = "item", name = lamp_name, amount = 1} }
-recipe.localised_name = {"entity-name."..lamp_name}
-recipe.localised_description = {"entity-description."..lamp_name}
-recipe.ingredients =
-    {
-      {"stone-furnace", 1},
-      {"iron-gear-wheel", 3},
-      {"iron-plate", 3}
-    }
-
-local item = table.deepcopy(data.raw.item[vanilla])
-item.name = lamp_name
-item.place_result = lamp_name
-item.localised_name = {"entity-name."..lamp_name}
-item.localised_description = {"entity-description."..lamp_name}
 
 local dark_sound =
 {
@@ -57,4 +32,5 @@ local ots45 = {
   filename = "__luxmortis__/img/intro.png"
 }
 
-data:extend({entity, item, recipe, dark_sound, pain, ots45})
+data:extend({dark_sound, pain, ots45})
+
